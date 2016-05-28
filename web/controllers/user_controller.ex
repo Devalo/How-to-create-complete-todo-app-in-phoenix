5596c194314599fefa2ef22db50f71118f345779
Stephan Valois <stephanvalois@gmail.com>
2016-05-28 22:47:25 +0200

    show action for user, with edit

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -24,4 +24,25 @@ defmodule Todo.UserController do
         |> render("new.html", changeset: changeset)
     end
   end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(User, id)
+    changeset = User.changeset(user)
+    render(conn, "show.html", user: user, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Repo.get(User, id)
+    changeset = User.changeset(user, user_params)
+
+    case Repo.update(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User updated")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> render("show.html", user: user, changeset: changeset)
+    end
+  end
 end
