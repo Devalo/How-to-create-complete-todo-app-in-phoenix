28df4119b38e8e07eabc9ed3a5b8e84063fd0b47
Stephan Valois <stephanvalois@gmail.com>
2016-05-28 23:17:37 +0200

    add comeonin and create registration changeset

--- mix.exs
+++ mix.exs
@@ -36,7 +36,8 @@ defmodule Todo.Mixfile do
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:comeonin, "~> 2.4"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
--- web/models/user.ex
+++ web/models/user.ex
@@ -15,4 +15,19 @@ defmodule Todo.User do
     |> validate_length(:password, min: 6)
     |> validate_format(:email, ~r/@/)
   end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+    end
+  end
 end
